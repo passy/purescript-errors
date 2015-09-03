d538b082645a340a12f4301c2e0723d6a14edf19
Pascal Hartig <phartig@twitter.com>
2015-09-03 23:17:56 +0200

    More tests

--- test/Main.purs
+++ test/Main.purs
@@ -5,6 +5,9 @@ import Data.Either (Either(..))
 import Data.Maybe (Maybe(..), isNothing)
 import Test.Unit
 import Control.Error.Util
+import Control.Monad.Except
+import Control.Monad.Maybe.Trans
+import Data.Identity
 
 main = runTest $ do
   test "hush" $ do
@@ -12,3 +15,30 @@ main = runTest $ do
       Just n -> n == 5
       _      -> false
     assert "Left is Nothing" $ isNothing (hush $ Left 5)
+
+  test "hushT" $ do
+    let exR = except $ Right "right"
+    let exL = except $ Left "left"
+
+    assert "Except Right is Just" $ do
+      let res = runIdentity <<< runMaybeT <<< hushT $ exR
+      case res of
+        Just str -> str == "right"
+        _        -> false
+
+    assert "Except Left is Nothing" $ do
+      let res = runIdentity <<< runMaybeT <<< hushT $ exL
+      isNothing res
+
+  test "note" $ do
+    assert "Nothing is Left a" $ do
+      let res = note "nothing" Nothing
+      case res of
+        Left a -> a == "nothing"
+        _      -> false
+
+    assert "Just is Right a" $ do
+      let res = note "nothing" $ Just "something"
+      case res of
+        Right a -> a == "something"
+        _       -> false
